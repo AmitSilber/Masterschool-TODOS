f909a3aa0a3b742b67450b5f18dd74e4eeeb4d1c
Amit Silber <amitsilber@gmail.com>
2023-05-10 18:47:48 +0300

    - changed localStorage structure from key value pairs to list - split render into different cell renders - added drag end to update order of list after drag is over - split init into two different inits - organized order of functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,67 +1,78 @@
+
 const todoTitle = document.querySelector("#todo-title");
 const todoDate = document.querySelector("#todo-duedate");
 const submit = document.querySelector("#todo-submit");
-const statusButton = document.querySelector("tbody tr td:last-child");
 const todoTable = document.querySelector("#todo-list-table");
+
+
 const checkIcon = "\u2713";
 const deleteIcon = "\u00D7";
 const editIcon = "\u270E";
+const offset = 2;
 
-var row;
+let rowToMove;
+let startDragRowIndex;
 
-/**
- * render (or reload) new item to table
- * @param {title,date,status,index} itemObj 
- */
-function renderItem(itemObj) {
-    const row = todoTable.insertRow(-1);
+// New todo Render
+function renderNewItem(itemObj) {
+    const newRowToInsert = todoTable.insertRow(-1);
+    const titleEdditButtonsCell = newRowToInsert.insertCell(0); // title + edit + delete buttons 
+    const dueDateCell = newRowToInsert.insertCell(1); // date 
+    const statusCell = newRowToInsert.insertCell(2); // status
+    renderTitleAndEditAndButtonsCell(titleEdditButtonsCell,itemObj.title);
+    renderDueDateCell(dueDateCell,itemObj.date);
+    renderStatusCell(statusCell,itemObj.status)
+}
 
-    const titleEdditButtonsCell = row.insertCell(0); // title + edit + delete buttons 
-    const dueDateCell = row.insertCell(1); // date 
-    const statusCell = row.insertCell(2); // status
+function renderStatusCell(cell,status){
+    cell.innerHTML = status ? checkIcon: "";
+    cell.className = "hover-cell";
+    cell.addEventListener("click", (event) =>updateStatus(event.target.parentElement));
+}
+
+function renderDueDateCell(cell,date){
+    cell.innerHTML = date;
+    cell.draggable = "true"; // enable row dragging by date column
+    cell.addEventListener("dragstart", (event) => startDrag(event));
+    cell.addEventListener("dragover", (event) => dragOver(event));
+    cell.addEventListener("dragend", (event) => endDrag(event));
+}
+
+function renderTitleAndEditAndButtonsCell(cell,title){
     const titleEditToggleCell = document.createElement("div") // title + edit wrapper
+    const editCell = document.createElement("span");
+    const deleteCell = document.createElement("span"); 
+
+    renderTitleAndEditCell(titleEditToggleCell,title);
+    deleteCell.textContent = deleteIcon;
+    editCell.className = "hover-cell"
+
+    editCell.textContent = editIcon
+    deleteCell.className = "hover-cell"
+ 
+    cell.appendChild(deleteCell);
+    cell.appendChild(editCell);
+    cell.appendChild(titleEditToggleCell);
+
+    deleteCell.addEventListener("click", (event) =>removeItem(event.target.parentElement.parentElement));
+    editCell.addEventListener("click", (event) => toggleEditMode(event.target.parentElement));
+
+}
+
+function renderTitleAndEditCell(cell,title){
     const editTitleModeCell = document.createElement("input"); // edit title slot
     const titleCell = document.createElement("p");
-    const deleteCell = document.createElement("span"); 
-    const editCell = document.createElement("span");
 
-    dueDateCell.draggable = "true"; // enable row dragging by date column
     editTitleModeCell.id = "edit-title" 
-    editTitleModeCell.style.setProperty("display", "none"); // make edit title invisible by default
-    row.setAttribute("key", itemObj.index);
     titleCell.id = "title-text"
-    statusCell.innerHTML = itemObj.status ? checkIcon: "";
-    titleCell.innerHTML = itemObj.title;
-    dueDateCell.innerHTML = itemObj.date;
-    editTitleModeCell.value = itemObj.title;
-    deleteCell.textContent = deleteIcon;
-    editCell.textContent = editIcon
-    statusCell.className = "hover-cell"
-    editCell.className = "hover-cell"
-    deleteCell.className = "hover-cell"
 
+    titleCell.innerHTML = title;
+    editTitleModeCell.value = title;
 
-
-    titleEditToggleCell.appendChild(editTitleModeCell);
-    titleEditToggleCell.appendChild(titleCell);
-  
-    titleEdditButtonsCell.appendChild(deleteCell);
-    titleEdditButtonsCell.appendChild(editCell);
-    titleEdditButtonsCell.appendChild(titleEditToggleCell);
-
-    dueDateCell.addEventListener("dragstart", (event) => startDrag(event));
-    dueDateCell.addEventListener("dragover", (event) => dragOver(event));
-
-
-    deleteCell.addEventListener("click", (event) =>
-        removeItem(event.target.parentElement.parentElement));
-
-    statusCell.addEventListener("click", (event) =>
-        updateStatus(event.target.parentElement));
-
-    editCell.addEventListener("click", (event) =>
-toggleEditMode(event.target.parentElement));
-
+    editTitleModeCell.style.setProperty("display", "none"); // make edit title invisible by default
+    
+    cell.appendChild(editTitleModeCell);
+    cell.appendChild(titleCell);
     editTitleModeCell.addEventListener("keypress", (event) => {
         if (event.key === "Enter") {
             event.preventDefault();
@@ -72,81 +83,16 @@ toggleEditMode(event.target.parentElement));
 
         }
     });
-
-
-}
-/**
- * 
- * @param {*} row - row in table HTML element 
- * @param {*} titleNode - title of task HTML element
- * @param {*} newTitle - new title to replace old one
- */
-function updateTitle(row, titleNode, newTitle) {
-    const key = row.getAttribute("key");
-    const itemToUpdate = JSON.parse(localStorage.getItem(key));
-    localStorage.removeItem(key);
-    itemToUpdate.title = newTitle;
-    localStorage.setItem(key, JSON.stringify(itemToUpdate));
-    titleNode.lastChild.innerHTML = newTitle;
-    toggleEditMode(row.firstChild); // change back to display mode
-
 }
 
 
-function createNewItem(title, date) {
-    let length = Number(localStorage.getItem("length"));
-    const newItem = {
-        title: title,
-        date: date,
-        status: false,
-        index: length++,
-    }
-    localStorage.setItem("length", length);
-    localStorage.setItem(newItem.index.toString(), JSON.stringify(newItem))
-    renderItem(newItem);
 
-}
-
-function removeItem(row) {
-    const key = row.getAttribute("key")
-    let length = localStorage.getItem("length");
-    let next = row
-    localStorage.removeItem(key);
-    for (let i = Number(key) + 1; i < length; i++) { // undate succesive rows
-        next = next.nextElementSibling;
-        updateItemIndex(i, next);
-    }
-    length--;
-    localStorage.setItem("length", length);
-
-    document.getElementById("todo-list-table").deleteRow(Number(key) + 2);
-}
-
-/**
- * change between edit and display mode
- * @param {*} titleNode  - container of elements to toggle
- */
-function toggleEditMode(titleNode) {
-    const toggleElement1 = titleNode.lastChild.childNodes[0];
-    const toggleElement2 = titleNode.lastChild.childNodes[1];
-    toggleElement1.style.display = toggleElement1.style.display === "none" ? "block" : "none"
-    toggleElement2.style.display = toggleElement2.style.display === "none" ? "block" : "none"
-
-}
-function updateItemIndex(key, itemElement) {
-    const item = JSON.parse(localStorage.getItem(key.toString()));
-    localStorage.removeItem(key.toString())
-    item.index--;
-    localStorage.setItem(item.index.toString(), JSON.stringify(item));
-    itemElement.setAttribute("key", item.index);
-
-}
-
+// todo operations:
 function updateStatus(row) {
-    const key = row.getAttribute("key");
-    const item = JSON.parse(localStorage.getItem(key));
-    item.status = !item.status; // update the status of the task (done or not)
-    localStorage.setItem(key, JSON.stringify(item));
+    const index = row.rowIndex - offset;
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    todos[index].status = !todos[index].status; // update the status of the task (done or not)
+    localStorage.setItem("todos", JSON.stringify(todos));
     if (row.lastElementChild.textContent) {
         row.lastElementChild.textContent = "";
     }
@@ -154,30 +100,101 @@ function updateStatus(row) {
         row.lastElementChild.textContent = checkIcon;
     }
 }
+
+function removeItem(row) {
+    const index = row.rowIndex - offset;
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    let length = Number(localStorage.getItem("length"));
+    todos.splice(index,1);
+    length--;
+    localStorage.setItem("length", length);
+    localStorage.setItem("todos",JSON.stringify(todos))
+    document.getElementById("todo-list-table").deleteRow(index + offset);
+    if(!length){
+        localStorage.removeItem("length")
+        localStorage.removeItem("todos")
+    }
+}
+
+function createNewItem(title, date) {
+    let length = Number(localStorage.getItem("length"));
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    const newItem = {
+        title: title,
+        date: date,
+        status: false,
+    }
+    localStorage.setItem("length", length+1);
+    todos.push(newItem)
+    localStorage.setItem("todos",JSON.stringify(todos));
+    renderNewItem(newItem);
+
+}
+
+function updateTitle(row, titleNode, newTitle) {
+    const index = row.rowIndex - offset;
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    todos[index].title = newTitle;
+    localStorage.setItem("todos", JSON.stringify(todos));
+    titleNode.lastChild.innerHTML = newTitle;
+    toggleEditMode(row.firstChild); // change back to display mode
+
+}
+
+function toggleEditMode(titleNode) {
+    const toggleElement1 = titleNode.lastChild.childNodes[0];
+    const toggleElement2 = titleNode.lastChild.childNodes[1];
+    toggleElement1.style.display = toggleElement1.style.display === "none" ? "block" : "none"
+    toggleElement2.style.display = toggleElement2.style.display === "none" ? "block" : "none"
+
+}
+
+// Drag handlers
 function startDrag(event) {
-    row = event.target.parentElement; // mark row we want to move
+    rowToMove = event.target.parentElement; // mark row we want to move
+    startDragRowIndex = rowToMove.rowIndex-offset;
 }
 function dragOver(event) {
     const currentRow = event.target.parentElement
     event.preventDefault();
     let children = Array.from(currentRow.parentElement.children);
-    if (children.indexOf(currentRow) > children.indexOf(row)) // switch between rows
-        currentRow.after(row);
-    else
-        currentRow.before(row);
+    if (children.indexOf(currentRow) > children.indexOf(rowToMove)){ // switch between rows
+        currentRow.after(rowToMove);
+    }else{
+        currentRow.before(rowToMove);
+    }
 }
+
+function endDrag(event){
+    const localStorageTodos = JSON.parse(localStorage.getItem("todos"))
+    const currentRow = event.target.parentElement
+    const item = localStorageTodos[startDragRowIndex];
+    localStorageTodos.splice(startDragRowIndex, 1);
+    localStorageTodos.splice(currentRow.rowIndex-offset, 0, item);
+    localStorage.setItem("todos",JSON.stringify(localStorageTodos));
+}
+
+
+
 function init() {
-    if (localStorage.length === 0) {
-        localStorage.setItem("length", "0");
-        createNewItem("Hi! Im a new task on your todo list", "03-05-2023")
-        return;
+    if (!localStorage.length) {
+        createNewTodoList();
+    }else{
+        loadTodoListFromLocalStorage();
     }
-    const length = localStorage.getItem("length");
-    for (let key = 0; key < length; key++) {
-        renderItem(JSON.parse(localStorage.getItem(key.toString())));
-    
+
     }
-    
+
+function createNewTodoList(){
+    localStorage.setItem("length", 0);
+    localStorage.setItem('todos',JSON.stringify([]));
+    createNewItem("Hi! Im a new task on your todo list", "03-05-2023")
+
+}
+
+function loadTodoListFromLocalStorage(){
+    const localStorageTodos = JSON.parse(localStorage.getItem("todos"));
+    localStorageTodos.forEach(item => renderNewItem(item))
 }
 
 submit.addEventListener("click", () => {
